39e6b3fce39440075d908b5e0b7986a5b256adb7
ShivaniKaruru7 <96602796+ShivaniKaruru7@users.noreply.github.com>
2022-06-04 13:16:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function setup() {
+    canvas = createCanvas (640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if(status !="")
+    {
+        for (i = 0; < object.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Detected";
+        }
+    }
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("FF0000");
+    rect(30, 60, 450, 350 );
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        consolr.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
